bda5e90553efcc0858d8dfd87e98ab6033f49446
John Larkin <john@johnjlarkin.com>
2017-04-26 20:18:22 -0400

    updated mdn class

--- MDNClass.py
+++ MDNClass.py
@@ -27,53 +27,61 @@ class MDN:
 		# self.x = x = tf.placeholder(tf.float32, shape = [None, self.STEP*self.ndim]) 
 		self.x = x = input_data_from_lstms
 
+		# NOTE: not doing this for the combination of LSTM + MDN
 		# Set up the hidden layer
-		# wh = tf.Variable(tf.random_normal([self.STEP * self.ndim, self.NHIDDEN]), name='wh')
-		wh = tf.Variable(tf.random_normal([self.STEP, self.NHIDDEN], dtype = d_type), name='wh')
-		bh = tf.Variable(tf.random_normal([self.NHIDDEN], dtype = d_type), name='bh')
+		# # wh = tf.Variable(tf.random_normal([self.STEP * self.ndim, self.NHIDDEN]), name='wh')
+		# wh = tf.Variable(tf.random_normal([self.STEP, self.NHIDDEN], dtype = d_type), name='wh')
+		# bh = tf.Variable(tf.random_normal([self.NHIDDEN], dtype = d_type), name='bh')
 
-		# Add dropout
-		# wh = tf.nn.dropout(wh, keep_prob = 1.0)
+		# # Add dropout
+		# # wh = tf.nn.dropout(wh, keep_prob = 1.0)
 
-		# Train
-		temp_h = tf.add(tf.matmul(x, wh), bh)
-		self.h = h = tf.nn.tanh(temp_h) # Shape (?, hidden) = (?, 30)
+		# # Train
+		# temp_h = tf.add(tf.matmul(x, wh), bh)
+		# self.h = h = tf.nn.tanh(temp_h) # Shape (?, hidden) = (?, 30)
 
 		''' Here, we are building the MDN'''
 		# Setting up the Pi's (they are one dimensional)
-		w_pi = tf.Variable(tf.random_normal([self.NHIDDEN, self.NCOMPONENTS], dtype = d_type), name = 'w_pi')
-		b_pi = tf.Variable(tf.random_normal([self.NCOMPONENTS], dtype = d_type), name = 'b_pi')
-		p = tf.add(tf.matmul(self.h, w_pi),b_pi)
+		w_pi = tf.get_variable(name = 'w_pi', initializer = tf.random_normal([final_dimension_from_lstm, self.NCOMPONENTS], dtype = d_type))
+		b_pi = tf.get_variable(name = 'b_pi', initializer = tf.random_normal([self.NCOMPONENTS], dtype = d_type))
+		p = tf.add(tf.matmul(x, w_pi),b_pi)
 		pis = tf.exp(p)
 		pis = pis/tf.reshape(tf.reduce_sum(pis, axis=1), [-1, 1]) # Shape (?, number of components) = (?, 3)	
 
 		# Setting up the correlations
-		w_corr = tf.Variable(tf.random_normal([self.NHIDDEN, self.NCOMPONENTS], stddev = 0.2, dtype = d_type), name = 'w_corr')
-		b_corr = tf.Variable(tf.random_normal([self.NCOMPONENTS], stddev = 0.2, dtype = d_type), name = 'b_corr')
-		precorr = tf.add(tf.matmul(self.h,w_corr), b_corr)
+		w_corr = tf.get_variable(name = 'w_corr', initializer = tf.random_normal([final_dimension_from_lstm, self.NCOMPONENTS], stddev = 0.2, dtype = d_type))
+		b_corr = tf.get_variable(name = 'b_corr', initializer = tf.random_normal([self.NCOMPONENTS], stddev = 0.2, dtype = d_type))
+		precorr = tf.add(tf.matmul(x,w_corr), b_corr)
 		corr = tf.tanh(precorr) # Shape (?, number of components) = (?, 3)
 
 		# Setting up the means
 		# Changed!!: 
-		w_mu = tf.Variable(tf.random_normal([self.NHIDDEN, self.NCOMPONENTS*self.ndim], dtype = d_type), name = 'w_mu')
-		b_mu = tf.Variable(tf.random_normal([self.NCOMPONENTS*self.ndim], dtype = d_type), name = 'b_mu')
+		w_mu = tf.get_variable(name = 'w_mu', initializer = tf.random_normal([final_dimension_from_lstm, self.NCOMPONENTS*self.ndim], dtype = d_type))
+		b_mu = tf.get_variable(name = 'b_mu', initializer = tf.random_normal([self.NCOMPONENTS*self.ndim], dtype = d_type))
 
 		# no activation function for the mus 
-		mu = tf.add(tf.matmul(self.h,w_mu),b_mu) # Shape (?, number of comp * number of dimensions) = (?, 6)
+		mu = tf.add(tf.matmul(x,w_mu),b_mu) # Shape (?, number of comp * number of dimensions) = (?, 6)
 
 		# Setting up sigma and variance
 		# Changed!!:
-		w_sigma = tf.Variable(tf.random_normal([self.NHIDDEN, self.NCOMPONENTS*self.ndim], dtype = d_type), name = 'w_sigma')
-		b_sigma = tf.Variable(tf.random_normal([self.NCOMPONENTS*self.ndim], dtype = d_type), name = 'b_sigma')
-		sigma = tf.add(tf.matmul(self.h,w_sigma),b_sigma)
+		w_sigma = tf.get_variable(name = 'w_sigma', initializer = tf.random_normal([final_dimension_from_lstm, self.NCOMPONENTS*self.ndim], dtype = d_type))
+		b_sigma = tf.get_variable(name = 'b_sigma', initializer = tf.random_normal([self.NCOMPONENTS*self.ndim], dtype = d_type))
+		sigma = tf.add(tf.matmul(x,w_sigma),b_sigma)
 		sigma = tf.exp(sigma) # Shape (?, number of comp * number of dimensions) = (?, 6)
+
+		# end of stroke parameter
+		eos = tf.get_variable(name = 'eos', initializer = tf.random_normal([1], dtype = d_type))
+		self.eos = eos = tf.divide(1.0, tf.add(1.0,tf.exp(eos))) 
 
 
 		''' Here we are going to build the mixture probabilites'''
 		sum_of_pis = tf.reduce_sum(pis)
 
 		# Target values
-		self.actual = actual = tf.reshape(targets, [-1, 1, self.ndim])
+		print('targets shape: {}'.format(targets))
+		xy_targets = tf.slice(targets, [0, 0], [-1, 2])
+		eos_targets = tf.slice(targets, [0, 2], [-1, 1])
+		self.actual = actual = tf.reshape(xy_targets, [-1, 1, self.ndim])
 
 		# Need to first do some dimensionality manipulation
 		# specifically for mu and sigma
@@ -92,8 +100,6 @@ class MDN:
 		# so x is going to be like 150 x 2 and then mus is going to be 3 x 2
 		# actual - (?, 2, 1) 
 		# mu - (?, 6)
-		print('actual', actual)
-		print('mu', mu)
 		dev = tf.sub(actual,mu) # Shape: (?, 3, 2)
 		# Shape explanation: we have a bunch of sequences and we have x and y points
 		#					 for three different components
@@ -125,12 +131,22 @@ class MDN:
 		# Building conditional probability (eqn 23)
 		# overall mixture probabilities has shape [?]
 		N_by_pis = tf.mul(N,pis)
-		self.mixture_prob = mixture_prob = tf.reduce_sum(N_by_pis, axis=1)
+		self.N_by_pis = N_by_pis
+		
+		eos_operator = tf.mul(eos,eos_targets) + tf.mul(1.0-eos, 1.0-eos_targets)
+		self.eos_operator = eos_operator
+
+		N_by_pis_by_eos = tf.mul(N_by_pis, eos_operator)
+
+		self.mixture_prob = mixture_prob = tf.reduce_sum(N_by_pis_by_eos, axis=1)
 
 	def compute_loss(self):
 		mixture_prob = self.mixture_prob
-		loss = tf.reduce_sum( -tf.log(tf.maximum(mixture_prob,1e-20)))
-		return loss
+
+		negative_log_odds = -tf.log(tf.maximum(mixture_prob,1e-20)) 
+		loss = tf.reduce_sum(negative_log_odds)
+
+		return loss, negative_log_odds
 
 	def return_mixture_prob(self):
 		return self.mixture_prob
